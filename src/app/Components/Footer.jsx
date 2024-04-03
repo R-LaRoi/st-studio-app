1239ccc499086615724a585523b7cb0ff9aaa688
R. LaRoi <are@raewhoo.com>
2024-04-03 16:52:51 -0400

    Media Queries

---
+++ src/app/Components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+<section className="--footer">
+<footer className="relative pt-1 pb-1">
+      <div className="w-full lg:w-6/12 px-4">
+         <hr className="my-6 border-slate-100"/>
+        <h4 className="text-lg fonat-semibold text-slate-100"></h4>
+        <h5 className="text-md  text-slate-100">
+        Let's connect. Software Engineer based in New York.
+        </h5>
+      </div>
+      <div className="w-full lg:w-6/12 px-4">
+  
+             <div className="mt-3 lg:mb-0 mb-3">
+          <button className="bg-white text-slate-100 shadow-lg font-normal h-7 w-7  items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+    </button>
+    <button className="bg-white text-slate-100 shadow-lg font-normal h-7 w-7 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+    </button>
+    
+    <button className="bg-white text-slate-100 shadow-lg font-normal h-7 w-7 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
+
+</button>
+        </div>
+          </div><br/>
+
+
+</footer>
+</section>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 import useMousePosition from './Components/mousePosition';
 import ButtonMenu from './Components/ButtonMenu'
-
+import Footer from './Components/Footer'
 
 export default function Home() {
 
@@ -15,7 +15,8 @@ export default function Home() {
 // backgroundColor: '#eb4f34',
   return (
 
-    <>
+
+
 
     <main className='main'>
 
@@ -28,70 +29,26 @@ export default function Home() {
         transition={{ type: 'tween', ease: 'backOut', duration:0.3}}
       ></motion.div>
 
+<div class=" ">
 
+  <div class="flex flex-wrap items-center justify-between mx-auto p-3 z-99">
+    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+       <svg xmlns="http://www.w3.org/2000/svg" width="2.5rem" height="2.frem" viewBox="0 0 24 24">
+	<path fill="white" d="M9.234 15.23q-2.505 0-4.254-1.745q-1.75-1.745-1.75-4.25q0-2.506 1.746-4.255q1.745-1.75 4.251-1.75q2.506 0 4.255 1.746q1.749 1.745 1.749 4.251q0 2.506-1.746 4.255q-1.745 1.749-4.25 1.749m1.15 5.538q-.667 0-1.141-.474q-.475-.475-.475-1.141v-.477q0-.292.195-.488q.196-.195.494-.214q1.74-.077 3.24-.692q1.5-.616 2.735-1.85q1.234-1.235 1.85-2.735q.615-1.5.692-3.24q.02-.298.214-.494q.196-.195.488-.195h.477q.666 0 1.14.475q.475.474.475 1.14v8.77q0 .666-.474 1.14q-.475.475-1.141.475z" />
+</svg>
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+    </a>
+</div>
+</div>
     <h1 className='header-text'>In pursuit of joy {''} <br/> by design_
 
     </h1> 
  <h3 className="header-h3">Dynamic creative developer, conceptualizing and delivering user-friendly solutions.</h3>
 
 <ButtonMenu />
-
-
-{/* 
-
-
-<div className='btn btn-three'></div> */}
-
-
-
-  {/* <div class="container ">
-  <div class= "row">
-    <div class="col-sm-3">
-   <Link href ="/Menu/Projects/" className="move">
-
-    
-    <div className="container">
-      <div className="body">
-    <div className="textContainer">
-          <p className="primary">projects</p>
-          <p className="secondary">projects</p>
-        </div>
-      </div>
-    </div>
-</Link>
-    </div>
-    <div class="col-sm-3">
-      <Link href ="/Menu/Connect">
-     <div className="container" >
-      <div className="body">
-    <div className="textContainer">
-          <p className="primary">connect</p>
-          <p className="secondary">meet_rachel</p>
-        </div>
-      </div>
-    </div>
-</Link>
-    </div>
-    <div class="col-sm-3">
-      <Link href ="/Menu/Studio">
-     <div className="container" >
-      <div className="body">
-    <div className="textContainer">
-            <p className="primary">studio
-            <span style={{color: "#FFE627"}}>_</span> </p>
-          <p className="secondary">art&design</p>
-        </div>
-      </div>
-    </div>
-   </Link>
-
-    </div>
-  </div>
-</div> */}
-
-
+<Footer />
 
 </main>
-</>
+
   );
 }
