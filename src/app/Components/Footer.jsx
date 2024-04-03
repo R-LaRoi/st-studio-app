c90e2aa799cedc8477676d6e7bc75dd8669723cf
R. LaRoi <are@raewhoo.com>
2024-04-03 19:24:19 -0400

    Footer

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function Footer() {
   return (
-<section className="--footer">
+    
+<section className="--footer ">
+<div className="--svg">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1053d8" fill-opacity="1" d="M0,128L80,160C160,192,320,256,480,261.3C640,267,800,213,960,186.7C1120,160,1280,160,1360,160L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+  </div>
 <footer className="relative pt-1 pb-1">
       <div className="w-full lg:w-6/12 px-4">
          <hr className="my-6 border-slate-100"/>
