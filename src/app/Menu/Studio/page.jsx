5596543d255237ff9bee346df618fab6c80b4002
R. LaRoi <are@raewhoo.com>
2024-04-16 13:46:46 -0400

    Animated footer

--- src/app/Menu/Studio/page.jsx
+++ src/app/Menu/Studio/page.jsx
@@ -1,9 +1,16 @@
 import "./studio.css"
 import Navigation from "../../Components/Navigation"
+import wave from './wave-haikei.svg'
+import Footer from "../../Components/Footer"
+
+
 export default function Page ()  {
   
 return(
+<section>
+        <Navigation />
+        studio page
 
-
-
-        <Navigation />)}
+<Footer />
+    </section>     
+        )}
